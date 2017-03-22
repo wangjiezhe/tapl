01cd25720b7755056dc7e70b5922a6f45db1a786
wangjiezhe <wangjiezhe@gmail.com>
2017-03-22 20:51:12 +0800

    List cannot be defined

--- fullsimple/test.f
+++ fullsimple/test.f
@@ -42,3 +42,9 @@ plus =
 plus 10 0;
 plus 5 6;
 plus 0 12;
+
+/*
+NatList = <nil:Unit, cons:{Nat,NatList}>;
+nil = <nil=unit> as NatList;
+cons = lambda n:Nat. lambda l:NatList. <cons={n,l}> as NatList;
+*/
